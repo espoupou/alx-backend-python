51fba712c5599e86a844584f5ac0cdc84b90e407
espoupou <126259211+espoupou@users.noreply.github.com>
2024-03-20 00:49:51 +0000

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ run 10 times corotines """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ generato r """
     for _ in range(0, 10):
         await asyncio.sleep(1)
