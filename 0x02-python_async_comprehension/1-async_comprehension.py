f376abdc80e8f7ca6fddc06de68876e2b95c0fb6
espoupou <126259211+espoupou@users.noreply.github.com>
2024-03-20 00:50:56 +0000

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,6 @@ import asyncio
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """collect and return"""
     return [_ async for _ in async_generator()]
