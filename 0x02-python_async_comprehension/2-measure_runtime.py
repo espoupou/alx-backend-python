96bc97b1ddc155aafbdf74ba8c9faa63da53630a
espoupou <126259211+espoupou@users.noreply.github.com>
2024-03-21 00:50:57 +0000

    Update 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,7 +6,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """the core function"""
     start = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
